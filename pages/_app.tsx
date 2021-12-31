b5dcb9e0c353e1a9ed10ca5e60cf0f63ba24cbaa
mblazko-macwro <mblazko-macwro@mblazko-macwro.local>
2021-12-31 17:52:51 +0100

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Layout from './components/Layout/Index'
+import 'styles/globals.css'
+import Layout from 'components/Layout/Index'
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
